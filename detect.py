71ab63624beaae0a66563bd3f0a2afa7af63bf58
Senggani <sengganifs@gmail.com>
2024-12-15 04:57:45 +0700

    rework model naming

--- detect.py
+++ detect.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO
+import os
 
 # image source
 img = "./20241123_173039.jpg"
 
-face_model = YOLO("best.pt")
-face_model.export(format='ncnn')
+model_name = "yolo11n"
+model_directory = "./" + model_name + "_ncnn_model"
 
-ncnn_face_model = YOLO("best_ncnn_model")
+if not(os.path.isdir(model_directory)):
+    face_model = YOLO(model_name + ".pt")
+    face_model.export(format='ncnn')
+
+ncnn_face_model = YOLO(model_directory)
 results = ncnn_face_model.predict(source=img, project="images")
