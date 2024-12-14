12f44b61bc1497b829488803659761b95d687364
Senggani <sengganifs@gmail.com>
2024-12-15 05:03:51 +0700

    export directory

--- detect.py
+++ detect.py
@@ -7,9 +7,14 @@ img = "./20241123_173039.jpg"
 model_name = "yolo11n"
 model_directory = "./" + model_name + "_ncnn_model"
 
+export_directory = "./" + model_name + "_ncnn_model"
+
 if not(os.path.isdir(model_directory)):
     face_model = YOLO(model_name + ".pt")
     face_model.export(format='ncnn')
+    
+if not(os.path.isdir(export_directory)):
+    os.makedirs(export_directory)
 
 ncnn_face_model = YOLO(model_directory)
-results = ncnn_face_model.predict(source=img, project="images")
+results = ncnn_face_model.predict(source=img, project=export_directory)
